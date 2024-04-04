de9393bc7c4d5c798ef88644d62e364a9157bba6
Wes Talley <wesatalley@gmail.com>
2024-04-03 22:53:23 -0500

    Finished first iteration of login

--- client/nexus-now/src/App.jsx
+++ client/nexus-now/src/App.jsx
@@ -1,18 +1,15 @@
 import LoginComponent from "./pages/LoginComponent";
 import img from "./assets/network-bg.jpg"
 import "./App.css"
-import { ReactDOM } from "react-dom";
 //import components here
 
 function App() {
   return (
-    <>
-      <body className="vh-100 hero-image" style={{backgroundImage: `url(${img})`}}>
-        <div className="col-5 offset-7">
-          <LoginComponent />
-        </div>
-      </body>
-    </>
+    <div className="vh-100 hero-image" style={{ backgroundImage: `url(${img})` }}>
+      <div className="col-5 offset-7">
+        <LoginComponent />
+      </div>
+    </div>
   );
 }
 
--- client/nexus-now/src/pages/LoginComponent.jsx
+++ client/nexus-now/src/pages/LoginComponent.jsx
@@ -3,9 +3,20 @@ import "../App.css"
 export default function LoginComponent() {
   return (
     <>
-      <div className="login-back vh-100">
-        <div className="card">
-        <h3 className="d-flex justify-content-center">Login</h3>
+      <div className="d-flex login-back vh-100 align-items-center">
+        <div className="card d-flex col-10 offset-1 login-card-back">
+          <div className="card-header">
+            <h3 className="d-flex justify-content-center">Login</h3>
+          </div>
+          <div className="card-body">
+            <form > {/* Add 'action="" method=""' later when backend is in development! */}
+              <label htmlFor="txtUsername" className="form-label">Username</label>
+              <input id="txtUsername" type="email" className="form-control" placeholder="BlurryBackground" />
+              <label htmlFor="txtPassword" className="form-label">Password</label>
+              <input id="txtPassword" type="password" className="form-control" placeholder="BobbyDropTables!" />
+              <button className="btn col-6 offset-3 mt-2 btn-nexus-theme text-white" type="submit"><strong>Login</strong></button>
+            </form>
+          </div>
         </div>
       </div>
     </>
